d7ffc28cea6ce400236758d93d397a98fde9bf40
MAYURESH JAGTAP <90536196+MJ5141@users.noreply.github.com>
2022-01-09 13:34:12 +1100

    Add files via upload

---
+++ 06-objects/objects.js
@@ -0,0 +1,53 @@
+// The Recipe Card
+// Never forget another recipe!
+//
+// Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+// On separate lines (one console.log statement for each), log the recipe information so it looks like:
+// Mole
+// Serves: 2
+// Ingredients:
+// cinnamon
+// cumin
+// cocoa
+
+const myRecipe = {
+  title: "Pav Bhaji",
+  serves: 2,
+  ingredients: ["bread", "vegies", "butter", "spices"]
+};
+console.log(`${myRecipe.title}
+Serves: ${myRecipe.serves}
+Ingredients: ${myRecipe.ingredients}`);
+
+//
+//
+// The Reading List
+// Keep track of which books you read and which books you want to read!
+//
+// Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+// Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+// Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+const myBooks = [
+  {name: "If it's not forever it's not a love",
+   author: "Shreyas Rahane",
+   read: true},
+
+ {name: "Can love happen twice?",
+  author: "Ajinkya Iyer",
+  read: true},
+
+{name: "The life",
+ author: "Shree Sharma",
+ read: false}
+];
+
+for( let i=0; i < myBooks.length; i++){
+  const myBook = myBooks[i];
+
+  if (myBook.read === true){
+    console.log(`You already read ${myBook.name} by ${myBook.author} `);
+  } else {
+    console.log(`You haven't read ${myBook.name} by ${myBook.author} `);
+  }
+};
