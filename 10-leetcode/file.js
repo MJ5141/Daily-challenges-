32ca2bb0f4d90de3ad3e4a4184b6361c70ddff0b
MAYURESH JAGTAP <90536196+MJ5141@users.noreply.github.com>
2022-02-28 13:15:42 +1100

    Add files via upload

--- 10-leetcode/file.js
+++ 10-leetcode/file.js
@@ -153,3 +153,29 @@ function ListNode(val, next) {
     this.val = (val === undefined ? 0 : val)
     this.next = (next === undefined ? null : next)
 }
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+//
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+//
+// Return k after placing the final result in the first k slots of nums.
+//
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+
+var removeDuplicates = function (nums) {
+    // Length of the updated array
+    let count = 0;
+    // Loop for all the elements in the array
+    for (let i = 0; i < nums.length; i++) {
+        // If the current element is equal to the next element, we skip
+        if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+            continue;
+        }
+        // We will update the array in place
+        nums[count] = nums[i];
+        count++;
+    }
+    return count;
+};
