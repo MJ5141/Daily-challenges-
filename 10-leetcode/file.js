dbff07cefd3e791dcaa60fa69ee343379fbd0280
MAYURESH JAGTAP <90536196+MJ5141@users.noreply.github.com>
2022-03-14 22:14:09 +1100

    Add files via upload

--- 10-leetcode/file.js
+++ 10-leetcode/file.js
@@ -382,3 +382,29 @@ function getUpdatedString(s) {
     sb += "#";
     return sb;
 }
+
+
+// Given a string s consisting of upper/lower-case alphabets and empty space characters ‘ ‘, return the length of the last word in the string. If the last word does not exist, return 0.
+
+
+function lengthOfLastWord(a)
+{
+    let len = 0;
+
+    // String a is 'final'-- can
+    // not be modified So, create
+    // a copy and trim the
+    // spaces from both sides
+    x = a.trim();
+
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] == ' ') {
+            len = 0;
+        }
+        else {
+            len++;
+        }
+    }
+
+    return len;
+}
