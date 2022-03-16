7cca4ced6228f186f85b0da54bb6292bed2174ca
MAYURESH JAGTAP <90536196+MJ5141@users.noreply.github.com>
2022-03-17 01:33:57 +1100

    Add files via upload

--- 10-leetcode/file.js
+++ 10-leetcode/file.js
@@ -572,8 +572,6 @@ var divide = function (dividend, divisor) {
 
 
 
-
-
 var generateParenthesis = function (n) {
     // Resultant list
     const result = [];
@@ -597,3 +595,28 @@ function generate(result, s, open, close, n) {
         generate(result, s + ")", open, close + 1, n);
     }
 };
+
+
+// # The Cash Register
+//
+// Write a function called cashRegister that takes a shopping cart object. The object contains item names and prices (itemName: itemPrice). The function should return the total price of the shopping cart.
+// Example
+//
+// ```
+// // Input
+const cartForParty = {
+  banana: "1.25",
+  handkerchief: ".99",
+  Tshirt: "25.01",
+  apple: "0.60",
+  nalgene: "10.34",
+  proteinShake: "22.36"
+};
+
+let prices = Object.values(cartForParty);
+
+let sum = 0
+for(let i=0; i<prices.length; i++){
+  sum= sum + parseFloat(prices[i]);
+}
+console.log(sum);
