da1c0ba6f66e68d8ab75a25f0ef19bb64f2c413f
MAYURESH JAGTAP <90536196+MJ5141@users.noreply.github.com>
2022-03-29 15:30:31 +1100

    Add files via upload

--- 10-leetcode/file.js
+++ 10-leetcode/file.js
@@ -1131,3 +1131,43 @@ function getUpdatedString(s) {
     sb += "#";
     return sb;
 }
+
+
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+//
+// If there is no common prefix, return an empty string "".
+//
+// Constraints
+// 0 ≤ strs.length ≤ 200
+// 0 ≤ strs[i].length ≤ 200
+// strs[i] consists of only lower-case English letters.
+
+
+
+var longestCommonPrefix = function (strs) {
+    // Longest common prefix string
+    let longestCommonPrefix = "";
+    // Base condition
+    if (strs == null || strs.length == 0) {
+        return longestCommonPrefix;
+    }
+    // Find the minimum length string from the array
+    let minimumLength = strs[0].length;
+    for (let i = 1; i < strs.length; i++) {
+        minimumLength = Math.min(minimumLength, strs[i].length);
+    }
+    // Loop for the minimum length
+    for (let i = 0; i < minimumLength; i++) {
+        // Get the current character from first string
+        let current = strs[0][i];
+        // Check if this character is found in all other strings or not
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[j][i] != current) {
+                return longestCommonPrefix;
+            }
+        }
+        longestCommonPrefix += current;
+    }
+    return longestCommonPrefix;
+};
